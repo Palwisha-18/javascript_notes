7dec643487e8750bb585e58d28fc48dda3aea6b6
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2022-11-09 12:04:26 -0700

    Add Assignment Operators subsection

---
+++ 03_Operators/3_Assignment_Operators/assignment_operators.js
@@ -0,0 +1,15 @@
+// assignment operator
+
+let counter = 0; // assigns value 0 to variable counter
+
+counter = counter + 1; // counter = 1
+
+
+// chaining assignment operators
+
+let a = 10, b = 20, c = 30;
+a = b = c; // all variables are 30
+
+let a = 10, b = 20, c = 30;
+b = c; // b is 30
+a = b; // a is also 30 
