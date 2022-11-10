b25537749b8023a33834305c3eac73f10d862318
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2022-11-10 12:08:32 -0700

    All control flow staements and operators

--- 04_Control_Flow_Statements/README.md
+++ 04_Control_Flow_Statements/README.md
@@ -6,4 +6,22 @@
     * if a condition is `true`, executes the code block that follows `if` branch. Otherwise, executes `else` branch.
 - `if ... else ... if` statement:
     * checks multiple conditions and execute the corresponding block if a condition is `true`.
-- `?:`
+- `?:` ternary operator:
+    * alternative for `if ... else` statement to make code more concise and easy to read.
+- `switch ... case` statement:
+    * the `switch` statement evaluates an `expression`, compares its result with `case` values, and executes the statement associated with the matching `case` value.
+    * uses the strict comparison (`===`) to compare the expression with the case values.
+- `while` statement:
+    * executes a block as long as a condition is `true`.
+- `do ... while` statement:
+    * executes a block until a condition evaluates to `false`.
+    * executes the statement at least once before evaluating the expression.
+- `for` statement:
+    * used to create a loop that execute a block based using various options.
+- `break` statement:
+    * used to terminate a loop prematurely.
+    * when used in a nested loop, the break statement terminates the enclosing loop. To terminate the nested loop, you use a `label` statement.
+- `continue` statement:
+    * used to skip the current iteration of a loop.
+- `comma` seperator:
+    * takes two expressions and evaluates them from left to right, and returns the value of the right expression.
--- 04_Control_Flow_Statements/control_flow_statements.js
+++ 04_Control_Flow_Statements/control_flow_statements.js
@@ -50,3 +50,154 @@ if (month == 1) {
   monthName = 'Invalid month';
 }
 console.log(monthName); // Jun
+
+// using ternary operator to perform single statement
+
+let age = 18;
+let message;
+
+message = age >= 16 ? 'You can drive.' : 'You cannot drive.';
+
+console.log(message); // You can drive.
+
+
+// using ternary operator to perform multiple statements
+
+let authenticated = true;
+let nextURL = authenticated
+  ? (alert('You will redirect to admin area'), '/admin')
+  : (alert('Access denied'), '/403');
+
+// redirect to nextURL here
+console.log(nextURL); // '/admin'
+
+
+// multiple ternary operators
+
+let speed = 90;
+let message = speed >= 120 ? 'Too Fast' : speed >= 80 ? 'Fast' : 'OK';
+
+console.log(message); // Fast
+
+
+// switch case
+
+let day = 3;
+let dayName;
+
+switch (day) {
+  case 1:
+    dayName = 'Sunday';
+    break;
+  case 2:
+    dayName = 'Monday';
+    break;
+  case 3:
+    dayName = 'Tuesday';
+    break;
+  case 4:
+    dayName = 'Wednesday';
+    break;
+  case 5:
+    dayName = 'Thursday';
+    break;
+  case 6:
+    dayName = 'Friday';
+    break;
+  case 7:
+    dayName = 'Saturday';
+    break;
+  default:
+    dayName = 'Invalid day';
+}
+
+console.log(dayName); // Tuesday
+
+
+// while loop
+
+let count = 1;
+while (count < 10) {
+    console.log(count);
+    count +=2;
+} // 1 3 5 7 9
+
+
+// do ... while loop
+
+let count = 0;
+do {
+  console.log(count);
+  count++;
+} while (count < 5) // 0 1 2 3 4
+
+
+// simple for loop
+     //initializer; condition; iterator
+for (let i = 1; i < 5; i++) {
+  console.log(i);
+}  // 1 2 3 4
+
+
+// for loop without the initializer
+
+let j = 1;
+for (; j < 10; j += 2) {
+  console.log(j);
+} // 1 3 5 7 9
+
+
+// for loop without the condition
+
+for (let j = 1; ; j += 2) {
+  console.log(j);
+  if (j > 10) {
+    break;
+  }
+} // 1 3 5 7 9 11 
+
+// for loop statement without any expression
+
+let j = 1;
+for (;;) {
+  if (j > 10) {
+    break;
+  }
+  console.log(j);
+  j += 2;
+} // 1 3 5 7 9 
+
+
+// for loop without the loop body
+
+let sum = 0;
+for (let i = 0; i <= 9; i++, sum += i);
+console.log(sum); // 55
+
+
+// break statement
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+  if (i == 2) {
+    break;
+  }
+} // 0 1 2
+
+
+// continue statement
+
+for (let i = 0; i < 10; i++) {
+  if (i % 2 === 0) {
+    continue;
+  }
+  console.log(i);
+} // 1 3 5 7 9
+
+
+// comma operator
+
+let x = 10;
+let y = (x++, x + 1);
+
+console.log(x, y); // 11, 12
